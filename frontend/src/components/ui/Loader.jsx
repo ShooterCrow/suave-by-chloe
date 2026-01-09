7cce1e84708b8f3304516cc84413d86d26960116
ShooterCrow <cydollarsign1@gmail.com>
2026-01-09 07:14:12 +0100

    Update Persist login

--- frontend/src/components/ui/Loader.jsx
+++ frontend/src/components/ui/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = ({ bg = false }) => {
     return (
-        <div className={`${bg && "bg-gray-700 bg-opacity-50"} absolute inset-0 flex flex-col justify-center items-center z-50`}>
+        <div className={`${bg && "bg-transparent bg-opacity-50"} absolute inset-0 flex flex-col justify-center items-center z-50`}>
             <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mb-6">
             </div>
         </div>
